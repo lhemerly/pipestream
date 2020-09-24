8e7be395f529a9c9fbb4e35aed1e372a037f37c3
Luiz Hemerly <lechemerly@gmail.com>
2020-09-23 23:51:43 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import numpy as np
+
+
+def file_input(path):
+    # TODO: Read other file types
+    df = pd.read_csv(path)
+    return df
+
+def select(table,
+           columnList):
+    # TODO: Select any number of columns
+    return newTable
+
+def filterRows(table,
+               columns,
+               filter):
+    newTable = table[columns] == filter
+    return newTable
+
+def join(table1,
+         table2,
+         index):
+    newTable = table1.join(table2,
+                           index)
+    return newTable
+
+def newColumn(table,
+              columnName,
+              calc):
+    newTable = table[columnName] = calc
+    return newTable
+
+def sort(table,
+         columnName):
+    newTable = table.sort_values(by=columnName)
+    return newTable
+
+def replace(table,
+            toReplace,
+            values):
+    newTable = table.replace(toReplace,
+                             values)
+    return newTable
+
+class pipeStream:
+    pipeInput = {} # A pipeInput is an ordered dictionary of order:input (can be a list)
+    pipeMethod = {} # A pipeInput is an ordered dictionary of order:method
+
+def interpreter(pipeStream):
+
+    for k, v in pipeStream.pipeInput.items():
+        methodToCall = getattr(main, pipeStream.pipeMethod[k])(*pipeStream.pipeInput[k])
+
+
